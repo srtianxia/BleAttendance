e3c92cd6abbd15f4b4b86c44499cd7f6b436cbea
616764899 <a616764899@126.com>
2017-02-15 00:05:49 +0800

    修改了课表显示的bug

--- app/src/main/java/com/srtianxia/bleattendance/ui/course/CourseFragment.java
+++ app/src/main/java/com/srtianxia/bleattendance/ui/course/CourseFragment.java
@@ -104,23 +104,22 @@ public class CourseFragment extends BaseFragment implements CoursePresenter.ICou
 
         String[] str_weeks = getResources().getStringArray(R.array.course_weeks);
         String[] str_times = getResources().getStringArray(R.array.course_time_1);
-        String month = new SchoolCalendar(mWeek,1).getCurrentMonth()+"";
+        String month = new SchoolCalendar(mWeek, 1).getCurrentMonth() + "";
 
-        if (mWeek == 0){
+        if (mWeek == 0) {
             mWeekday.setVisibility(View.GONE);
-        }
-        else{
+        } else {
             mMonth.setText(month + "\n" + "月");
             mWeekday.setVisibility(View.VISIBLE);
         }
 
         for (int i = 0; i < 7; i++) {
             //添加日期
-            SchoolCalendar calendar = new SchoolCalendar(mWeek,i+1);
+            SchoolCalendar calendar = new SchoolCalendar(mWeek, i + 1);
             TextView tv_day = new TextView(getActivity());
-            LinearLayout.LayoutParams params_day = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1);
+            LinearLayout.LayoutParams params_day = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1);
             tv_day.setLayoutParams(params_day);
-            tv_day.setText(calendar.getCurrentDay()+"");
+            tv_day.setText(calendar.getCurrentDay() + "");
             tv_day.setTextColor(getResources().getColor(R.color.colorGrey_content));
             tv_day.setGravity(Gravity.CENTER);
             mWeekday.addView(tv_day);
@@ -133,17 +132,17 @@ public class CourseFragment extends BaseFragment implements CoursePresenter.ICou
             mWeeks.addView(tv);
         }
 
-        for(int i=0; i<12; i++){
+        for (int i = 0; i < 12; i++) {
             TextView time = new TextView(getContext());
             TextView course = new TextView(getContext());
-            time.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL);
+            time.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL);
             time.setText(str_times[i]);
             time.setTextColor(getResources().getColor(R.color.colorGrey_content));
-            time.setTextSize(TypedValue.COMPLEX_UNIT_SP,12);
-            course.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL);
-            course.setText((i+1)+"");
-            LinearLayout.LayoutParams time_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0,1);
-            LinearLayout.LayoutParams course_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0,1);
+            time.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
+            course.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+            course.setText((i + 1) + "");
+            LinearLayout.LayoutParams time_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1);
+            LinearLayout.LayoutParams course_lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1);
 
             time.setLayoutParams(time_lp);
             course.setLayoutParams(course_lp);
@@ -154,8 +153,8 @@ public class CourseFragment extends BaseFragment implements CoursePresenter.ICou
         }
 
         List<CourseTimeEntity> courseTimeEntities = new ArrayList<>();
-        for (int i = 0; i < 12; i++){
-            CourseTimeEntity temp = new CourseTimeEntity(str_times[i],(i+1)+"");
+        for (int i = 0; i < 12; i++) {
+            CourseTimeEntity temp = new CourseTimeEntity(str_times[i], (i + 1) + "");
             courseTimeEntities.add(temp);
         }
 
@@ -172,12 +171,12 @@ public class CourseFragment extends BaseFragment implements CoursePresenter.ICou
     }
 
     @Override
-    public void showCourse(List<Course> courses,String week) {
+    public void showCourse(List<Course> courses, String week) {
         List<Course> tempCourseList = new ArrayList<>();
         tempCourseList.addAll(courses);
         if (mCourseTableView != null) {
             mCourseTableView.clearList();
-            mCourseTableView.addContentView(tempCourseList,week);
+            mCourseTableView.addContentView(tempCourseList, week);
         }
     }
 

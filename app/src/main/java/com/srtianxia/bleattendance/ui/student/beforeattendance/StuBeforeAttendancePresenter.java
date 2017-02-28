9b5ec90542f91fec858e7b9e97ab0d0d49445b5a
616764899 <a616764899@126.com>
2017-02-28 20:43:38 +0800

    课表fragment实现懒加载，修改fragment重叠bug

--- app/src/main/java/com/srtianxia/bleattendance/ui/student/beforeattendance/StuBeforeAttendancePresenter.java
+++ app/src/main/java/com/srtianxia/bleattendance/ui/student/beforeattendance/StuBeforeAttendancePresenter.java
@@ -15,13 +15,13 @@ import java.util.List;
 /**
  * Created by 梅梅 on 2017/2/11.
  */
-public class StuBeforeAttendancePresenter extends BasePresenter<StuBeforeAttendancePresenter.IStuBeforeAttView>{
+public class StuBeforeAttendancePresenter extends BasePresenter<StuBeforeAttendancePresenter.IStuBeforeAttView> {
 
     private Api mApi;
 
     public StuBeforeAttendancePresenter(IStuBeforeAttView baseView) {
         super(baseView);
-        mApi = ApiUtil.createApi(Api.class,ApiUtil.getBaseUrl());
+        mApi = ApiUtil.createApi(Api.class, ApiUtil.getBaseUrl());
     }
 
     @Override
@@ -29,46 +29,46 @@ public class StuBeforeAttendancePresenter extends BasePresenter<StuBeforeAttenda
         return getView();
     }
 
-    public void requestStuAttForNet(){
+    public void requestStuAttForNet() {
         getView().loading();
-        String token = PreferenceManager.getInstance().getString(PreferenceManager.SP_TOKEN_STUDENT,"");
-        mApi.getStuAttendanceInfo(token,0,0,0)
+        String token = PreferenceManager.getInstance().getString(PreferenceManager.SP_TOKEN_STUDENT, "");
+        mApi.getStuAttendanceInfo(token, 0, 0, 0)
                 .compose(RxSchedulersHelper.io2main())
-                .subscribe(this::requestStuAttSuccess,this::requestStuAttFailure);
+                .subscribe(this::requestStuAttSuccess, this::requestStuAttFailure);
     }
 
-    private void requestStuAttSuccess(StuAttInfoEntity entity){
+    private void requestStuAttSuccess(StuAttInfoEntity entity) {
         List<StuAttInfoEntity.ShowData> dataList = stuAttInfoFiler(entity);
         getView().loadData(dataList);
         getView().loadFinish();
     }
 
-    private void requestStuAttFailure(Throwable throwable){
+    private void requestStuAttFailure(Throwable throwable) {
         getView().loadFailure(throwable.toString());
         getView().loadFinish();
     }
 
-    private List<StuAttInfoEntity.ShowData> stuAttInfoFiler(StuAttInfoEntity entity){
+    private List<StuAttInfoEntity.ShowData> stuAttInfoFiler(StuAttInfoEntity entity) {
 
         List<StuAttInfoEntity.ShowData> dataList = new ArrayList<>();
         List<String> stringList = new ArrayList<>();
-        HashMap<String,Integer> map = new HashMap();
+        HashMap<String, Integer> map = new HashMap();
 
-        for (int i=0; i<entity.data.size(); i++){
-            if (!map.containsKey(entity.data.get(i).jxbID)){
-                map.put(entity.data.get(i).jxbID,0);
+        for (int i = 0; i < entity.data.size(); i++) {
+            if (!map.containsKey(entity.data.get(i).jxbID)) {
+                map.put(entity.data.get(i).jxbID, 0);
                 stringList.add(entity.data.get(i).jxbID);
             }
         }
 
-        for (int j=0; j<entity.data.size(); j++){
-            if (map.containsKey(entity.data.get(j).jxbID) && entity.data.get(j).status.equals("1")){
+        for (int j = 0; j < entity.data.size(); j++) {
+            if (map.containsKey(entity.data.get(j).jxbID) && entity.data.get(j).status.equals("1")) {
                 int tempInt = map.get(entity.data.get(j).jxbID) + 1;
-                map.put(entity.data.get(j).jxbID,tempInt);
+                map.put(entity.data.get(j).jxbID, tempInt);
             }
         }
 
-        for (int k=0; k<map.size(); k++){
+        for (int k = 0; k < map.size(); k++) {
             StuAttInfoEntity.ShowData tempData = new StuAttInfoEntity.ShowData();
             tempData.jxbID = stringList.get(k);
             tempData.att_num = map.get(stringList.get(k));
@@ -79,8 +79,11 @@ public class StuBeforeAttendancePresenter extends BasePresenter<StuBeforeAttenda
 
     public interface IStuBeforeAttView extends BaseView {
         void loadData(List<StuAttInfoEntity.ShowData> dataList);
+
         void loadFailure(String throwable);
+
         void loadFinish();
+
         void loading();
     }
 }
